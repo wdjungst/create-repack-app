60af710fe331edcabe5d644f6bef9e773b3317e0
David Jungst <djungst@gmail.com>
2017-11-30 15:15:45 -0700

    fix render where rails injects mystery css on refresh

--- example/base/controllers/static_controller.rb
+++ example/base/controllers/static_controller.rb
@@ -1,6 +1,8 @@
 require 'rails/application_controller'
 
 class StaticController < Rails::ApplicationController
+  layout false
+
   def index
     render file: Rails.root.join('public', 'index.html')
   end
