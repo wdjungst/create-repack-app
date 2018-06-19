08958f623f8563d0f573c8c098fb42fba6f1adfc
David Jungst <djungst@gmail.com>
2018-06-19 16:17:42 -0600

    move flash reducer to constants

--- example/client/src/reducers/flash.js
+++ example/client/src/reducers/flash.js
@@ -2,11 +2,11 @@ const SET_FLASH = 'SET_FLASH';
 const CLEAR_FLASH = 'CLEAR_FLASH';
 
 export const setFlash = (message, color) => {
-  return { type: 'SET_FLASH', message, color };
+  return { type: SET_FLASH, message, color };
 };
 
 export const clearFlash = () => {
-  return { type: 'CLEAR_FLASH' };
+  return { type: CLEAR_FLASH };
 };
 
 export default (state = {}, action) => {
